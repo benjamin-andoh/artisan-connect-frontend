cb73a34ffa8508a64a2b327b8cc92960c666bff0
benooandoh <benoo.andoh@gmail.com>
2025-06-13 21:27:04 -0400

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import ArtisanDashboard from './pages/artisan/ArtisanDashboard ';
 import CustomerDashboard from './pages/customer/CustomerDashboard';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AdminDashboard />} />
-      <Route path="/artisan/dashboard" element={<ArtisanDashboard />} />
-      <Route path="/customer/dashboard" element={<CustomerDashboard />} />
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/artisan/dashboard" element={<ArtisanDashboard />} />
+        <Route path="/customer/dashboard" element={<CustomerDashboard />} />
+      </Routes>
+
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-500 text-white p-4">
       <ul className="flex gap-6">
-        <li><Link to="/admin/dashboard">Admin</Link></li>
-        <li><Link to="/artisan/dashboard">Artisan</Link></li>
-        <li><Link to="/customer/dashboard">Customer</Link></li>
+        <li><Link to="/admin/adminDashboard">Admin</Link></li>
+        <li><Link to="/artisan/artisanDashboard">Artisan</Link></li>
+        <li><Link to="/customer/customerDashboard">Customer</Link></li>
       </ul>
     </nav>
   );
